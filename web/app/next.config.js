4b0ce1dcac978fb6dd680eadf401683b1b483541
Adam Prokop <prokoad1@cvut.cz>
2026-05-09 12:08:51 +0200

    Odstraneni Matomo

--- web/app/next.config.js
+++ web/app/next.config.js
@@ -10,8 +10,6 @@ const nextConfig = {
     },
     env: {
         api: process.env.API_URL ?? 'http://127.0.0.1:8000',
-        MATOMO_URL: process.env.MATOMO_URL,
-        MATOMO_SITE_ID: process.env.MATOMO_SITE_ID,
     },
     // Docker configuration
     output: "standalone",
